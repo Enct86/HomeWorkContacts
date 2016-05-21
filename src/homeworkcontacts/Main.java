669a32e275e14e8802ff0e0230b4ec293637e05c
Max Savchenko <enct86@gmail.com>
2016-05-21 07:25:15 +0300

    FileNotFoundException(

--- src/homeworkcontacts/Main.java
+++ src/homeworkcontacts/Main.java
@@ -7,6 +7,8 @@ package homeworkcontacts;
 
 import java.util.Scanner;
 
+import java.io.*;
+
 /**
  *
  * @author Max Savchenko
@@ -15,20 +17,57 @@ public class Main {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
         Scanner scr1 = new Scanner(System.in);
         int i = 0;
-        do { 
-            System.out.printf("1 - Add new contact, \n2 - View all contacts\n3 - Edit contact \n4 - Delete contact\n5 - Exit");
+        File contactdir = new File("contacts");
+
+        if (!contactdir.exists()) {
+            contactdir.mkdir();
+        }
+        do {
+            System.out.printf("1 - Add new contact, \n2 - View all contacts\n3 - Show contact \n4 - Delete contact\n5 - Exit\n");
             String input1 = scr1.nextLine();
             i = Integer.parseInt(input1);
-            switch(i){
+            switch (i) {
                 case 1:
-                    
+                    long namelong = System.currentTimeMillis();
+                    String namestring = Long.toString(namelong);
+                    File filecontact = new File("./" + contactdir + "./" + namestring + ".txt");
+                    filecontact.createNewFile();
+                    Scanner scandata = new Scanner(System.in);
+                    FileOutputStream fos = new FileOutputStream(filecontact, true);
+                    System.out.printf("Enter - NAME\nTelephone number\nEmail\nSkype\nAt When finish type end\n");
+                    while (true) {
+                        String line = scandata.nextLine();
+                        if ("end".equals(line)) {
+                            break;
+                        }
+                        byte[] bytes = line.getBytes();
+                        fos.write(bytes);
+                        fos.write('\n');
+                    }
+                    fos.close();
+                case 2:
+                    try {
+                        FileInputStream fis = new FileInputStream(contactdir);
+                        File[] listFiles = contactdir.listFiles();
+                        if (listFiles != null) {
+                            for (File file : listFiles) {
+                                if (file.isFile()) {
+                                    System.out.printf(file.getName());
+                                }
+                            }
+                        }
+                    } catch (FileNotFoundException e) {
+                        System.err.println("Error" + e);
+                    }
             }
-            
+
         } while (i != 5);
     }
-    
+
 }
